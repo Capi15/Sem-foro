678ed0cb0dd90355505a67b867193834f37d5718
Capi15 <capicapi1515@gmail.com>
2020-06-05 18:28:28 +0100

    mudando para listas

--- src/semaforo/Tabuleiro.java
+++ src/semaforo/Tabuleiro.java
@@ -5,28 +5,30 @@ package semaforo;
  * @author bruno
  */
 public class Tabuleiro {
+
     //atributos
     private final int totalPecas;
     private final int pecasVerdes;
     private final int pecasAmarelas;
     private final int pecasVermelhas;
     //private ListaLigada<Peca> casa[][] = new ListaLigada[3][4]; //criação do array bidimensional(matriz)
-    private final String casa[][] = new String[3][4];
-    
+    private final ListaLigada<Peca> casa = new ListaLigada<>();
+
     //construtor da classe Tabuleiro
-    public Tabuleiro(){
+    public Tabuleiro() {
         this.totalPecas = 24;
         this.pecasVerdes = this.pecasAmarelas = this.pecasVermelhas = 8;
+        inserePecas();
     }
-    
+
     //este método é responsável por imprimir o tabuleiro para o ecrã
-    public String mostraTabuleiro(){
+    public String mostraTabuleiro() {
         //percorre o array por linha
-        for (int linha = 0; linha < 3; linha++){
+        for (int linha = 0; linha < 3; linha++) {
             System.out.println("-----------------");
             //percorre o array por colunas
-            for(int coluna = 0; coluna < 4; coluna++){
-                System.out.printf("| " + casa[linha][coluna] + " ");
+            for (int coluna = 0; coluna < 4; coluna++) {
+                System.out.printf("| " +  + " ");
             }
             System.out.println("|");
         }
@@ -34,9 +36,37 @@ public class Tabuleiro {
         //não há retorno, portanto é retornado null
         return null;
     }
-    
-    public void escolha(int li, int co){
+
+    public void inserePecas() {
+        //percorre o array por linha
+        for (int linha = 0; linha < 3; linha++) {
+            //percorre o array por colunas
+            for (int coluna = 0; coluna < 4; coluna++) {
+                casa.Push(new Peca(null, linha, coluna));
+            }
+        }
+    }
+
+    public Peca procuraPeca(int linha, int coluna){
+         
+    }
+
+public void escolha(int li, int co) {
         ListaLigada<Peca> listaPecas = new ListaLigada<>();
         casa[li][co] = listaPecas.Push(new Peca(CoresEnum.VERDE)).toString();
+//        if (casa[li][co] == listaPecas.Push(new Peca(CoresEnum.VERDE)).toString()) {
+//            casa[li][co] = listaPecas.Push(new Peca(CoresEnum.AMARELO)).toString();
+//        } else {
+//            return;
+//        }
+//
+//        if (casa[li][co] == listaPecas.Push(new Peca(CoresEnum.AMARELO)).toString()) {
+//            casa[li][co] = listaPecas.Push(new Peca(CoresEnum.VERMELHO)).toString();
+//        }else{
+//           return;
+//        } 
+
     }
+    
+    public void criaLista
 }
